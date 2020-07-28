29deeb49b786bf783ab5ecdb781f1d7e718fca21
William Cheng <wing328hk@gmail.com>
2020-07-28 11:58:20 +0800

    regenerate with 2.1.0 as version

--- R/api_client.R
+++ R/api_client.R
@@ -39,7 +39,7 @@ ApiClient  <- R6::R6Class(
     # base path of all requests
     basePath = "https://v2.namsor.com/NamSorAPIv2",
     # user agent in the HTTP request
-    userAgent = "OpenAPI-Generator/1.0.0/r",
+    userAgent = "OpenAPI-Generator/2.1.0/r",
     # default headers in the HTTP request
     defaultHeaders = NULL,
     # username (HTTP basic authentication)
