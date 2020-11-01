156be4e96c04580254dea3ef91703838fc62a952
NamSor SAS <contact@namsor.com>
2020-11-01 12:06:00 +0100

    Minor update

--- run-R.bash
+++ run-R.bash
@@ -2,7 +2,7 @@ echo update openapi-generator
 ./bin/utils/openapi-generator-cli.sh
 rm -Rf ./namsor/client/R/*
 echo run openapi-generator
-java -jar modules/openapi-generator-cli/target/openapi-generator-cli.jar generate  --git-repo-id namsor-r-sdk2 --git-user-id namsor --skip-validate-spec -i https://v2.namsor.com/NamSorAPIv2/api2/openapi.json -g r -o  namsor/client/R --additional-properties packageName=namsor,packageVersion=2.1.0
-cp -R /home/namsor/codegen/openapi-generator/namsor/client/R/* /home/namsor/codegen/namsor-R-sdk2/
-cp /home/namsor/codegen/openapi-generator/run-R.bash /home/namsor/codegen/namsor-R-sdk2/
+java -jar modules/openapi-generator-cli/target/openapi-generator-cli.jar generate  --git-repo-id namsor-python-sdk2 --git-user-id namsor --skip-validate-spec -i https://v2.namsor.com/NamSorAPIv2/api2/openapi.json -g r -o  namsor/client/R -DpackageName=namsor
+cp -R /home/namsor/codegen/openapi-generator/namsor/client/R/* /home/namsor/codegen/namsor-r-sdk2/
+cp /home/namsor/codegen/openapi-generator/run-R.bash /home/namsor/codegen/namsor-r-sdk2/
 
